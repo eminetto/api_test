196cc533873d889ec5deef5b9feaa41266a1c899
Nathalia <nathalia.lcz@hotmail.com>
2021-02-23 23:25:13 -0300

    db working with user table, registering new user with first constraint

--- api/api.py
+++ api/api.py
@@ -1,60 +1,45 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from flask_marshmallow import Marshmallow
 import os, jwt
 from datetime import datetime
 
 
 app = Flask(__name__)
-basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'crud.sqlite')
 db = SQLAlchemy(app)
-ma = Marshmallow(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 
 
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     displayName = db.Column(db.String(80), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True)
-    password = db.Column(db.String(100), unique=False, nullable=False)
-    image = db.Column(db.String(500), unique=False)
+    # password = db.Column(db.String(100), unique=False)
+    # image = db.Column(db.String(500), unique=False, nullable=True)
 
 
-    def __init__(self, username, email):
-        self.displayName = username
+    def __init__(self, displayName, email):
+        self.displayName = displayName
         self.email = email
 
-    # @validates('email')
-    # def validate_email(self, key, address):
-    #     assert '@' in address
-    #     return address
 
-class UserSchema(ma.Schema):
-    class Meta:
-        # Fields to expose
-        fields = ('displayName', 'email')
-
-
-user_schema = UserSchema()
-users_schema = UserSchema(many=True)
-
-class Posts(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(80), unique=False)
-    content = db.Column(db.String(120), unique=True)
-    userId = db.Column('users',
-        db.Integer,
-        db.ForeignKey('users.id'),
-        nullable=False
-    )
-    users = db.relationship("Users", backref="post")
-    published = db.Column(db.Date, default=datetime.utcnow())
-    updated = db.Column(db.Date)
-
-
-    def __init__(self, username, email):
-        self.title = username
-        self.userId = email
+# class Posts(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     title = db.Column(db.String(80), unique=False)
+#     content = db.Column(db.String(120), unique=True)
+#     userId = db.Column('users',
+#         db.Integer,
+#         db.ForeignKey('users.id'),
+#         nullable=False
+#     )
+#     users = db.relationship("Users", backref="post")
+#     published = db.Column(db.Date, default=datetime.utcnow())
+#     updated = db.Column(db.Date)
+#
+#
+#     def __init__(self, title, userId):
+#         self.title = username
+#         self.userId = userId
 
 
 @app.route("/")
@@ -64,41 +49,47 @@ def hello():
 # endpoint to create new user
 @app.route("/user", methods=["POST"])
 def add_user():
-    displayName = request.json['displayName']
-    email = request.json['email']
+    data = request.get_json()
+
     if len(data['displayName'])<=8:
-        return {
+        return jsonify({
             'error': 'Bad Request',
             'message': 'Name field needs to be bigger than 8 characters'
-        }, 400
+        }), 400
 
-    new_user = Users(displayName, email)
+
+    new_user = Users(
+        displayName= data['displayName'],
+        email = data['email']
+    )
 
     db.session.add(new_user)
     db.session.commit()
 
-    return jsonify(new_user)
+    return {
+        'id': new_user.id,
+        'displayName': new_user.displayName,
+        'email': new_user.email
+    }, 201
 
 
-
-def encode_auth_token(self, user_id):
-    """
-    Generates the Auth Token
-    :return: string
-    """
-    try:
-        payload = {
-            'exp': datetime.datetime.utcnow() + datetime.timedelta(days=0, seconds=5),
-            'iat': datetime.datetime.utcnow(),
-            'sub': user_id
-        }
-        return jwt.encode(
-            payload,
-            app.config.get('SECRET_KEY'),
-            algorithm='HS256'
-        )
-    except Exception as e:
-        return e
+#     """
+#     Generates the Auth Token
+#     :return: string
+#     """
+#     try:
+#         payload = {
+#             'exp': datetime.datetime.utcnow() + datetime.timedelta(days=0, seconds=60),
+#             'iat': datetime.datetime.utcnow(),
+#             'sub': user_id
+#         }
+#         return jwt.encode(
+#             payload,
+#             app.config.get('SECRET_KEY'),
+#             algorithm='HS256'
+#         )
+#     except Exception as e:
+#         return e
 
 
 if __name__ == '__main__':
